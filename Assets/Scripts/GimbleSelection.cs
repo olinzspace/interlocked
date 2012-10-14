6a923111aedefd04f755586a8bfdb9c20ae0b63c
zspace <zspace@lists.olin.edu>
2012-10-14 11:30:36 -0400

    Started rewriting MouseGrab, got really messy and buggy. Currently selection works but no applying forces.

---
+++ Assets/Scripts/GimbleSelection.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class GimbleSelection : MonoBehaviour {
+	private Vector3 scale;
+	public Vector3 direction;
+	// Use this for initialization
+	void Start () {
+		scale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Hover () {
+		transform.localScale = 2*scale;
+	}
+}
--- Assets/Scripts/MouseGrab.cs
+++ Assets/Scripts/MouseGrab.cs
@@ -6,102 +6,122 @@ public class MouseGrab : MonoBehaviour {
 	public float dampingConstant = 10.0f;
 	public float rayLength = 10.0f;
 	public float maxForce = 10.0f;
-	public Camera mainCamera;
+	
 	public GameObject gimble;
+	public Camera mainCamera;
 	
-	private bool buttonPrev = false;
-	private GameObject selected;
-	private GameObject collided;
+	private GameObject selectedGameobject;
 	private float selectedObjectDistance;
-	private Vector3 selectedObjectHitPos;
-	
-	void Start () {
+	private Vector3 selectedHitPos;
+	private Vector3 gimbleHitPos;
+	private Vector3 direction = Vector3.zero;
+	Ray pointerRay;
 
+	private bool buttonPrev = false;
+	
+	void Start() {
+		
 	}
 	
-	void Update () {
-		ClearBlockHighlighting();
-		Ray pointerRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-		
-
-		RaycastHit collidedRaycast = GetCollidedRaycast(pointerRay);
-		
-		if (collidedRaycast.collider && !selected) {
-			collidedRaycast.collider.gameObject.SendMessageUpwards ("AddHoverHighlight");
-		}
+	void Update() {
+		pointerRay = mainCamera.ScreenPointToRay(Input.mousePosition);
 		
 		bool buttonCurrent = Input.GetMouseButton(0);
-		
 		if (buttonCurrent && !buttonPrev) {
-			ButtonJustPressed(collidedRaycast);
+			if (!selectedGameobject) {
+				UpdateSelected();
+			} else {
+				UpdateDirection();
+				if (direction == Vector3.zero) {
+					UpdateSelected();
+				}
+			}
+
+			
+			if (selectedGameobject) {
+				ShowGimble(selectedHitPos);
+			} else {
+				HideGimble();
+			}
 		} else if (buttonCurrent) {
-			ButtonPressed(pointerRay);
+			UpdateForce();
+
 		} else if (!buttonCurrent && buttonPrev) {
-			ButtonJustReleased(collidedRaycast);
+		
 		}
 		buttonPrev = buttonCurrent;
 	}
 	
-	RaycastHit GetCollidedRaycast(Ray pointerRay) {
+	void UpdateForce() {
+		/*
+		if (selectedGameobject) {
+			selectedGameobject.GetComponent<BlockSelection>().AddSelectedHighlight();
+		
+			Vector3 diff = pointerRay.GetPoint(selectedObjectDistance)+selectedHitPos-selectedGameobject.transform.position;
+			diff = new Vector3(diff.x*direction.x, diff.y*direction.y, diff.z*direction.z);
+			Vector3 force = diff * springConstant - dampingConstant*selectedGameobject.rigidbody.velocity;
+			if (force.magnitude > maxForce) {
+				force = maxForce*force.normalized;	
+			}
+			//selected.rigidbody.AddForce(force);
+			//gimble.transform.position = selectedGameobject.transform.position-selectedObjectHitPos;
+		}	*/
+	}
+		
+	void UpdateSelected() {
+		RaycastHit[] allCollidedRaycasts = Physics.RaycastAll (pointerRay);
+		selectedGameobject = null;
+		selectedObjectDistance = -1.0f;
+		
+		// Find the collided object that's closest to the origin of the ray
+		for (int i=0; i < allCollidedRaycasts.Length; i++) {
+			if (!allCollidedRaycasts[i].collider || allCollidedRaycasts[i].collider.gameObject.layer == 8)
+				continue;
+			//Vector3 objectHitPos = allCollidedRaycasts[i].collider.gameObject.transform.root.transform.position - pointerRay.origin;
+			Vector3 objectHitPos = allCollidedRaycasts[i].point - pointerRay.origin;
+			if (objectHitPos.magnitude < selectedObjectDistance || selectedObjectDistance == -1.0) {
+				selectedGameobject = allCollidedRaycasts[i].collider.gameObject;
+				selectedObjectDistance = objectHitPos.magnitude;
+				selectedHitPos = allCollidedRaycasts[i].point;
+			}
+		}
+	}
+	
+	void ClearSelected() {
+		selectedGameobject = null;
+	}
+	void UpdateDirection() {
 		RaycastHit[] allCollidedRaycasts = Physics.RaycastAll (pointerRay);
 		
-		RaycastHit collidedRaycast = new RaycastHit();
+		GameObject gameobj = null;
 		float shortestDistance = -1.0f;
 		
 		// Find the collided object that's closest to the origin of the ray
 		for (int i=0; i < allCollidedRaycasts.Length; i++) {
-			if (!allCollidedRaycasts[i].collider)
+			if (!allCollidedRaycasts[i].collider
+				|| null == allCollidedRaycasts[i].collider.gameObject.GetComponent<GimbleSelection>()) //on top layer
 				continue;
 			//Vector3 objectHitPos = allCollidedRaycasts[i].collider.gameObject.transform.root.transform.position - pointerRay.origin;
 			Vector3 objectHitPos = allCollidedRaycasts[i].point - pointerRay.origin;
 			if (objectHitPos.magnitude < shortestDistance || shortestDistance == -1.0) {
-				collidedRaycast = allCollidedRaycasts[i];
+				gameobj = allCollidedRaycasts[i].collider.gameObject;
 				shortestDistance = objectHitPos.magnitude;
+				gimbleHitPos = allCollidedRaycasts[i].collider.gameObject.transform.root.transform.position - allCollidedRaycasts[i].point;
 			}
 		}
-		
-		return collidedRaycast;
-	}
-	
-	void ButtonJustPressed(RaycastHit raycastHit) {
-		if (!raycastHit.collider) {
-			return;
-		}
-		selected = raycastHit.collider.gameObject.transform.root.gameObject;
-		selectedObjectDistance =  raycastHit.distance;
-		selectedObjectHitPos = raycastHit.collider.gameObject.transform.root.transform.position - raycastHit.point;
-		selected.rigidbody.isKinematic = false;	
-		gimble.SetActiveRecursively(true);
-		gimble.transform.position = raycastHit.point;
-	}
-		
-	void ButtonJustReleased(RaycastHit raycastHit) {
-		gimble.SetActiveRecursively(false);
-		if (selected) {
-			selected.rigidbody.isKinematic = true;
-			selected = null;
-		}
-	}
-	void ButtonPressed(Ray pointerRay) {
-		if (selected) {
-			
-			selected.GetComponent<BlockSelection>().AddSelectedHighlight();
-			Vector3 diff = pointerRay.GetPoint(selectedObjectDistance)+selectedObjectHitPos - selected.transform.position;
-			Vector3 force = diff * springConstant - dampingConstant*selected.rigidbody.velocity;
-			if (force.magnitude > maxForce) {
-				force = maxForce*force.normalized;	
-			}
-			selected.rigidbody.AddForce(force);
-			
+		if (gameobj) {
+			direction = gameobj.GetComponent<GimbleSelection>().direction;
+		} else {
+			direction = Vector3.zero;	
 		}
 	}
 	
-	void ClearBlockHighlighting() {
-		Object[] objects = FindObjectsOfType(typeof(BlockSelection));
-		foreach (Object block in objects) {
-			BlockSelection blockSelection = block as BlockSelection;
-			blockSelection.RemoveHighlight();
-		}	
+	void ShowGimble(Vector3 point) {
+		gimble.SetActiveRecursively(true);	
+		gimble.transform.position = point;
+	}
+	
+	void HideGimble() {
+		gimble.SetActiveRecursively(false);	
 	}
 }
-
