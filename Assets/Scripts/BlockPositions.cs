07da870fd1a7d89d3c97b87206069dfe6e905561
zspace <zspace@lists.olin.edu>
2012-10-23 23:09:13 -0400

    Removed unnecessary code from BlockPositions

--- Assets/Scripts/BlockPositions.cs
+++ Assets/Scripts/BlockPositions.cs
@@ -2,45 +2,28 @@ using UnityEngine;
 using System.Collections;
 
 public class BlockPositions : MonoBehaviour {
-	public float maxForce = 125.0f;
 	public float nearOriginalPosRange = 5.0f;
+	public float tollerance = 0.01f;
 
-	
-	public Vector3 originalPos, lastPos;
-	public bool snapbackForce;
+	private Vector3 originalPos;
 	
 	// Use this for initialization
 	void Start () {
 		originalPos = transform.position;
-		lastPos = originalPos;
-		snapbackForce = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (AtOriginalPos () || !IsNearOriginalPos ()) {
-			snapbackForce = false;
-			gameObject.GetComponent<BlockSelection>().RemoveHighlight();
-		}
-		if (snapbackForce) {
-			Vector3 diff = transform.position - originalPos;
-			Vector3 force = - maxForce * diff.normalized;
-			gameObject.rigidbody.AddForce (force);
-		}
+		
 	}
 	
 	public bool AtOriginalPos() {
 		Vector3 diff = transform.position - originalPos;
-		return diff.magnitude <= 1.0e-2; // Tollerance
+		return diff.magnitude <= tollerance;
 	}
 	
 	public bool IsNearOriginalPos() {
 		Vector3 diff = transform.position - originalPos;
 		return diff.magnitude <= nearOriginalPosRange;
 	}	
-	
-	public bool WasNearOriginalPos() {
-		Vector3 diff = lastPos - originalPos;
-		return diff.magnitude <= nearOriginalPosRange;
-	}
 }
