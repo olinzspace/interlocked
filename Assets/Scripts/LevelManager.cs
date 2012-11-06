35ed4e0ad6c86cb66099b6db6f27b3df229153b3
zspace <zspace@lists.olin.edu>
2012-11-06 18:50:50 -0500

    Added logging for number of selection for each individual piece

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -14,12 +14,12 @@ public class LevelManager : MonoBehaviour {
 	private String userId;
 	private int numSelectEvents, numMouseEvents;
 	private TimeSpan firstPieceLiberationTime;
-	
+	private int[] numSelectByPiece;
+
 	// Use this for initialization
 	void Start () {
-		numSelectEvents = 0;
-		numMouseEvents = 0;
 		firstPieceLiberationTime = new TimeSpan(0);
+		numSelectByPiece = new int[pen.GetComponent<PenGrab>().puzzlePieces.Length];
 		
 		sw = new Stopwatch();
 		sw.Start();
@@ -30,7 +30,8 @@ public class LevelManager : MonoBehaviour {
 	
 	}
 	
-	public void IncrNumSelectEvents() {
+	public void IncrNumSelectEvents(int pieceIndex) {
+		numSelectByPiece[pieceIndex]++;
 		numSelectEvents++;
 	}
 	
@@ -70,6 +71,18 @@ public class LevelManager : MonoBehaviour {
 		
 		bool logExists = File.Exists (filename);
 		using (StreamWriter writer = new StreamWriter (filename, true)) {
+			
+			string numSelectByPieceStr = "";
+			for (int i=0; i<numSelectByPiece.Length; i++) {
+				numSelectByPieceStr += numSelectByPiece[i];
+				if (i != numSelectByPiece.Length) {
+					numSelectByPieceStr += ", ";
+				}
+			}
+			for (int i=numSelectByPiece.Length; i<8; i++) {
+				numSelectByPieceStr += ", --";
+			}
+			
 			if (!logExists) {
 				writer.Write (	"UserID, " + 
 							  	"Date, " +
@@ -77,7 +90,15 @@ public class LevelManager : MonoBehaviour {
 								"TotalElapsedTime, " + 
 								"NumSelectionEvents, " +
 								"NumMouseEvents, " + 
-								"FirstPieceLiberationTime" + "\n");
+								"FirstPieceLiberationTime, " + 
+								"Piece1Selections, " +
+								"Piece2Selections, " +
+								"Piece3Selections, " +
+								"Piece4Selections, " +
+								"Piece5Selections, " +
+								"Piece6Selections, " +
+								"Piece7Selections, " +
+								"Piece8Selections" + "\n");
 								
 			}
 			writer.Write (	userId + ", " + 
@@ -86,7 +107,8 @@ public class LevelManager : MonoBehaviour {
 							FormattedTimeString(totalElapsedTime) + ", " +
 							numSelectEvents + ", " +
 							numMouseEvents + ", " + 
-							FormattedTimeString(firstPieceLiberationTime) + "\n");
+							FormattedTimeString(firstPieceLiberationTime) + ", " +
+							numSelectByPieceStr + "\n");
 		}
 		
 		Application.LoadLevel (nextLevel);
--- Assets/Scripts/PenGrab.cs
+++ Assets/Scripts/PenGrab.cs
@@ -81,12 +81,20 @@ public class PenGrab : MonoBehaviour {
 			return;
 		}
 		
-		// Increase logger's count for number of selections
-		levelManager.GetComponent<LevelManager>().IncrNumSelectEvents();
 		
 		selected = raycastHit.collider.gameObject.transform.root.gameObject;
 		selected.rigidbody.drag = 0;
 //		selected.GetComponent<BlockPositions>().lastPos = selected.transform.position;
+		
+		// Increase logger's count for number of selections
+		int pieceIndex = -1;
+		for (int i=0; i<puzzlePieces.Length; i++) {
+			if (selected == puzzlePieces[i]) {
+				pieceIndex = i;
+				break;
+			}
+		}
+		levelManager.GetComponent<LevelManager>().IncrNumSelectEvents(pieceIndex);
 		
 		selectedObjectDistance =  raycastHit.distance;
 		selectedObjectHitPos = raycastHit.collider.gameObject.transform.root.transform.position - raycastHit.point;
